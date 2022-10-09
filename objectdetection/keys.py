94877647ffa7939d342fdd174f1efc70916cfceb
Daniel Mironiuk (RIT Student) <dm9375@rit.edu>
2022-10-09 01:22:28 -0400

    Completed SMS api integration

---
+++ objectdetection/keys.py
@@ -0,0 +1,5 @@
+account_sid = 'AC50e63f097e72b7852ddc06ae5fe08347'
+auth_token = '94c248886440e7ae2df6265ca8e9e517'
+
+twilio_number = '+19259403510'
+target_number = '+18454994070'
--- objectdetection/main.py
+++ objectdetection/main.py
@@ -1,40 +1,70 @@
+from tracemalloc import stop
+import keys
 import cv2
+from twilio.rest import Client
+from stopwatch import Stopwatch
+import datetime
+
+def send_sms():
+    ct = datetime.datetime.now()
+    # Your Account SID from twilio.com/console
+    account_sid = keys.account_sid
+    # Your Auth Token from twilio.com/console
+    auth_token  = keys.auth_token
+
+    client = Client(account_sid, auth_token)
+
+    message = client.messages.create(
+        to=keys.target_number, 
+        from_=keys.twilio_number,
+        body="There is a Person Detected by your camera at " + str(ct))
+
+    print(message.sid)
+
+def capture_images():
+    cap = cv2.VideoCapture(0)
+    cap.set(3, 640)
+    cap.set(4, 480)
+
+    classNames = []
+    classFile = 'objectdetection/coco.names'
+    with open(classFile, 'rt') as f:
+        classNames = f.read().rstrip('\n').split('\n') 
+
+    configPath = 'objectdetection/ssd_mobilenet_v3_large_coco_2020_01_14.pbtxt'
+    weightsPath = 'objectdetection/frozen_inference_graph.pb'
+
+    net = cv2.dnn_DetectionModel(weightsPath, configPath)
+    net.setInputSize(320,320)
+    net.setInputScale(1.0/127.5)
+    net.setInputMean((127.5, 127.5, 127.5))
+    net.setInputSwapRB(True)
+    
+    stopwatch = Stopwatch(0)
+    stopwatch.start()
+    while True:
+        try: 
+            success, img = cap.read()
+            classIds, confs, bbox = net.detect(img, confThreshold=0.5)
+            print(classIds, bbox)
+            if len(classIds) != 0:
+                for classId, confidence, box in zip(classIds.flatten(), confs.flatten(), bbox):
+                    cv2.rectangle(img, box, color=(0,255,0), thickness=2)
+                    cv2.putText(img, classNames[classId-1].upper(), (box[0]+10, box[1]+30), cv2.FONT_HERSHEY_COMPLEX,1,(0,255,0),2)
+                    cv2.putText(img, str(round(confidence*100, 2)),(box[0]+150, box[1]+30), cv2.FONT_HERSHEY_COMPLEX,1,(0,255,0),2)
+                    if classNames[classId-1].upper() == "PERSON" and confidence*100>70:
+                        if int(stopwatch.duration) > 60: 
+                            send_sms()
+                            stopwatch.reset()
+                            stopwatch.start()
+            cv2.imshow('Object Detector', img)
+            cv2.waitKey(1)
+        except:
+            continue
+
+def main():
+    capture_images()
 
 
-cap = cv2.VideoCapture(0)
-cap.set(3, 640)
-cap.set(4, 480)
-
-classNames = []
-classFile = 'objectdetection/coco.names'
-with open(classFile, 'rt') as f:
-    classNames = f.read().rstrip('\n').split('\n') 
-
-configPath = 'objectdetection/ssd_mobilenet_v3_large_coco_2020_01_14.pbtxt'
-weightsPath = 'objectdetection/frozen_inference_graph.pb'
-
-net = cv2.dnn_DetectionModel(weightsPath, configPath)
-net.setInputSize(320,320)
-net.setInputScale(1.0/127.5)
-net.setInputMean((127.5, 127.5, 127.5))
-net.setInputSwapRB(True)
-
-while True:
-    try: 
-        success, img = cap.read()
-        classIds, confs, bbox = net.detect(img, confThreshold=0.5)
-        print(classIds, bbox)
-        if len(classIds) != 0:
-            for classId, confidence, box in zip(classIds.flatten(), confs.flatten(), bbox):
-                cv2.rectangle(img, box, color=(0,255,0), thickness=2)
-                cv2.putText(img, classNames[classId-1].upper(), (box[0]+10, box[1]+30), cv2.FONT_HERSHEY_COMPLEX,1,(0,255,0),2)
-
-                cv2.putText(img, str(round(confidence*100, 2)),(box[0]+150, box[1]+30), cv2.FONT_HERSHEY_COMPLEX,1,(0,255,0),2)
-                if classNames[classId-1].upper() == 'PERSON':
-
-
-        cv2.imshow('Output', img)
-        cv2.waitKey(1)
-    except:
-        continue
-
+if __name__ == "__main__":
+    main()
