8f9acb459c7f05ba68a4a5d2a3a79e2e2aa61b77
Daniel Mironiuk (RIT Student) <dm9375@rit.edu>
2022-10-16 16:01:22 -0400

    Starting async uploading

--- objectdetection/main.py
+++ objectdetection/main.py
@@ -116,6 +116,18 @@ def post_video():
         os.remove(os.path.join(data_file_folder, file))
 
 
+def async_post_video():
+    client_s3 = boto3.client('s3', 
+    aws_access_key_id=os.environ.get("AWS_ACCESS_KEY"),
+    aws_secret_access_key=os.environ.get("AWS_SECRET_KEY"))
+
+    data_file_folder = os.path.join(os.getcwd(), 'objectdetection/Video')
+    for file in os.listdir(data_file_folder):
+        if not file.startswith('~'):
+            client_s3.upload_file(os.path.join(data_file_folder, file),
+            os.environ.get("AWS_SECURITY_BUCKET_NAME"), file)
+        os.remove(os.path.join(data_file_folder, file))
+
 def main():
     capture_images()
     
